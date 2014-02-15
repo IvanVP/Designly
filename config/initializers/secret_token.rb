41b6c357016ce2489d9923d8ea698d907fe4e6aa
IvanVP <ipetrakov@gmail.com>
2014-02-15 21:54:02 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Designly::Application.config.secret_key_base = '0eeea6432017a35c1dde448a7de059ee05837c976f48f8e2858eb2bccd296b23692995ef5c9f5714cd76cfd20be8d9ae1cb4ee08f78401729c398ec2e4e8ae3d'
